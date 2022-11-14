5095232f0416eaa64bd4845a15c8970f4e1e13a4
Shamo <76420983+shamonaghiyev@users.noreply.github.com>
2022-11-14 13:07:56 +0400

    Upload Script Js file

---
+++ task2/script.js
@@ -0,0 +1,34 @@
+class Message {
+    constructor(authorName, time, text) {
+        this.authorName = authorName
+        this.time = time   
+        this.text = text
+    }
+
+    toString() {
+         console.log(`${this.authorName} --->  ${this.time}:${this.text}`)
+    }
+}
+
+
+class Messenger {
+
+    constructor() {
+        this.messages = []
+    }
+
+    show_history() {
+        this.messages.map(message => message.toString())
+    }
+
+    send(author, text) {
+        let message = new Message(author, text, new Date().getHours() + ':' + new Date().getMinutes())
+        this.messages.push(message)
+    }
+}
+
+
+let messenger = new Messenger()
+messenger.send('Adil', 'ilk mesaj')
+messenger.send('Məryəm', 'İkinci mesaj')
+messenger.show_history()
